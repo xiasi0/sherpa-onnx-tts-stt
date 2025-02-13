41dbe0254a9e3d6b3088aa31044a12660a66c68e
ptbsare <496725701@qq.com>
2025-02-13 20:29:24 +0800

    add GPU Dockerfile support

--- sherpa-onnx-tts-stt/CHANGELOG.md
+++ sherpa-onnx-tts-stt/CHANGELOG.md
@@ -13,3 +13,10 @@
 
 - Bug Fix: Fix kokora-tts_v1 tts.
 - 修复kokora-tts_v1 bug
+
+## 0.2.9
+
+- Add GPU Support. (See Dockerfile.gpu)
+- fix custom_models for addon
+- 实验性添加GPU支持。（见Dockerfile.gpu）
+- 修复addon的custom_models参数
--- sherpa-onnx-tts-stt/run.py
+++ sherpa-onnx-tts-stt/run.py
@@ -366,10 +366,10 @@ async def main() -> None:
                 vocoder=os.path.join(tts_model_dir,"hifigan_v2.onnx"),
                 lexicon=os.path.join(tts_model_dir, cli_args.tts_model, "lexicon.txt"),
                 tokens=os.path.join(tts_model_dir, cli_args.tts_model, "tokens.txt"),
-
+                data_dir=os.path.join(tts_model_dir, "espeak-ng-data"),
                 dict_dir=os.path.join(tts_model_dir, cli_args.tts_model, "dict"),
                 ),
-                provider="cpu",    # or "cuda" if you have a GPU
+                provider='cpu',    # or "cuda" if you have a GPU
                 num_threads=cli_args.tts_thread_num,     # Adjust as needed
                 debug=cli_args.debug,       # Set to True for debugging output
                 ),
@@ -394,7 +394,7 @@ async def main() -> None:
                 data_dir=os.path.join(tts_model_dir, cli_args.tts_model, "espeak-ng-data"),
                 dict_dir=os.path.join(tts_model_dir, cli_args.tts_model, "dict"),
                 ),
-                provider="cpu",
+                provider='cpu',
                 num_threads=cli_args.tts_thread_num,
                 debug=cli_args.debug,
                 ),
